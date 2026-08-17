7420578bcd6ccb2456bfdffbe0c74240e5444318
Garrincha077 <kemija077@gmail.com>
2026-08-18 01:03:20 +0200

    feat: surface original methodology beside discovery terminal

--- frontend/src/Root.tsx
+++ frontend/src/Root.tsx
@@ -1,6 +1,7 @@
 import {useState} from 'react'
 import DeepVueTerminal from './DeepVueTerminal'
 import GroupsPage from './GroupsPage'
+import OriginalEngineDock from './OriginalEngineDock'
 import {resetPanelSizes,useResizablePanels} from './useResizablePanels'
 import './resizable-panels.css'
 
@@ -9,7 +10,7 @@ export default function Root(){
   useResizablePanels()
   const openTicker=(ticker:string)=>{location.hash=ticker;setView('terminal')}
   return <>
-    {view==='groups'?<GroupsPage onBack={()=>setView('terminal')} onOpenTicker={openTicker}/>:<><DeepVueTerminal/><button className="dv-groups-launch" onClick={()=>setView('groups')}>◎ Groups</button></>}
+    {view==='groups'?<GroupsPage onBack={()=>setView('terminal')} onOpenTicker={openTicker}/>:<><DeepVueTerminal/><OriginalEngineDock/><button className="dv-groups-launch" onClick={()=>setView('groups')}>◎ Groups</button></>}
     <button className="ss-layout-reset" onClick={resetPanelSizes} title="Reset all resized panels to their default size">↺ Reset layout</button>
   </>
 }
