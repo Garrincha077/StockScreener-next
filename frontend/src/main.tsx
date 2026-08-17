7825997f872e91b5cb43e9fb3f906ae5fd8569b0
Garrincha077 <kemija077@gmail.com>
2026-08-17 22:17:07 +0200

    style: load data-first terminal overrides

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './terminal.css'
+import './datafirst.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
